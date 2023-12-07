b49ad4be17c5d53a50e7f07267884ffc48cbebfe
hadson0 <hadsonsb2221@gmail.com>
2023-12-07 11:24:42 -0300

    Bug fix

--- gui/main.py
+++ gui/main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import serial
+import re
 
 SERIAL_PORT = '/dev/ttyUSB0'
 SERIAL_BAUDRATE = 9600
@@ -27,29 +28,35 @@ total_number = tk.Label(root)
 total_number.pack()
 
 
-
 def update_interface():
     ser = serial.Serial(SERIAL_PORT, SERIAL_BAUDRATE, timeout=0.1)
     ser.write(b'd')
     
-    response = ser.readline()
+    try:
+        response = ser.readline()
+    except:
+        return
     print(response)
+
+    root.after(100, update_interface)
+    if response == b'':
+        return
     
-    if response != b'':
-        response = response.strip().decode('utf-8')
-        aux = response.split(";")
-        if len(aux) != 3:
-            return
-        
-        queue_str, current, total = aux
-        queue = [number for number in queue_str]
+    response = response.decode('utf-8')
+
+    pattern = r"data:(.*?);(.*?);(.*?)"
+    match = re.match(pattern, response)
+
+    if match:
+        queue = list(match.group(1))
+        current = match.group(2)
+        total = match.group(3)
 
         team_queue_label.config(text="\n".join(queue))                                
         current_team.config(text=current)
         total_number.config(text=total)
 
         ser.close()
-        root.after(100, update_interface)               # Atualiza a interface a cada 100ms
 
 if __name__ == '__main__':
     update_interface()
